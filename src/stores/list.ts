e686e1102f4f40313b0488fa9461e0a89982b208
norami <norami.dream@gmail.com>
2020-09-08 10:33:03 +0900

    In-place modification of Firestore collection

--- src/stores/list.ts
+++ src/stores/list.ts
@@ -10,18 +10,41 @@ interface DocData<T> {
   id: string;
   document: T;
 }
+
+const toDocData = <T>(doc: firestore.QueryDocumentSnapshot): DocData<T> => ({
+  document: doc.data() as T,
+  id: doc.id
+});
+
 export class List<T> {
-  items: null | DocData<T>[] = null;
+  items: null | DocData<T>[] = [];
   [refField]: firestore.CollectionReference;
   [unregisterField]: () => void;
   constructor(ref: firestore.CollectionReference) {
     this[refField] = ref;
     this[unregisterField] = ref.onSnapshot(
       (snapshot: firestore.QuerySnapshot) => {
-        this.items = (snapshot.docs || []).map(doc => ({
-          document: doc.data() as T,
-          id: doc.id
-        }));
+        for (const docChange of snapshot.docChanges()) {
+          if (docChange.type === "added") {
+            if (docChange.oldIndex === -1) {
+              this.items?.push(toDocData<T>(docChange.doc));
+            } else {
+              this.items?.splice(
+                docChange.oldIndex,
+                0,
+                toDocData<T>(docChange.doc)
+              );
+            }
+          } else if (docChange.type === "modified") {
+            this.items?.splice(
+              docChange.oldIndex,
+              1,
+              toDocData<T>(docChange.doc)
+            );
+          } else if (docChange.type === "removed") {
+            this.items?.splice(docChange.oldIndex, 1);
+          }
+        }
         console.log("snapshot", this.items);
       }
     );
