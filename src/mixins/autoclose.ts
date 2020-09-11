efe45ebc4676067f3155a3300e001ab11fc20820
norami <norami.dream@gmail.com>
2020-09-11 16:10:28 +0900

    Improvement for Typescript consistency

--- src/mixins/autoclose.ts
+++ src/mixins/autoclose.ts
@@ -1,35 +1,40 @@
+const unregisterMapField = Symbol("autoclose");
 declare module "vue/types/options" {
   interface ComponentOptions<V extends Vue> {
     autoclose?: string[];
   }
 }
-
-const filedPrefix = "__autoclose_";
+declare module "vue/types/vue" {
+  interface Vue {
+    [unregisterMapField]: Record<string, { close: () => void }>;
+  }
+}
 
 export const autoclose = {
   created(this: Vue) {
-    const { autoclose } = this.$options;
-    const thisAny = this as any;
-    for (const field of autoclose || []) {
+    const { autoclose: autoCloseFields } = this.$options;
+    this[unregisterMapField] = {};
+    const unregisterMap = this[unregisterMapField];
+    for (const field of autoCloseFields || []) {
       this.$watch(
         field,
         (newValue, _oldValue) => {
-          console.log("closing by watch", field);
-          const autoCloseField = filedPrefix + field;
-          thisAny[autoCloseField]?.close();
-          thisAny[autoCloseField] = newValue;
+          if (unregisterMap[field]) {
+            console.log("closing by watch", field);
+          }
+          unregisterMap[field]?.close();
+          unregisterMap[field] = newValue;
         },
         { immediate: true }
       );
     }
   },
   beforeDestroy(this: Vue) {
-    const { autoclose } = this.$options;
-    const thisAny = this as any;
-    for (const field of autoclose || []) {
-      const autoCloseField = filedPrefix + field;
+    const { autoclose: autoCloseFields } = this.$options;
+    const unregisterMap = this[unregisterMapField];
+    for (const field of autoCloseFields || []) {
       console.log("closing by beforeDestroy");
-      thisAny[autoCloseField]?.close();
+      unregisterMap[field]?.close();
     }
   }
 };
